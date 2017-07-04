55f4feed5f0a10f3d9588af246e563e49e51da53
maxxflyer <maxxflyer@gmail.com>
2017-07-04 14:37:09 +0200

    Create l.sol

---
+++ l.sol
@@ -0,0 +1,49 @@
+contract r is SuperOwned{
+address c;
+uint p;
+uint peri;
+uint end;
+address temp;
+
+function r(address con,uint pri,uint per){
+c=con;
+p=pri;
+peri=per;
+}
+
+function setPrice(uint pri) onlyOwner{
+p=pri;
+}
+
+function setPeriod(uint per) onlyOwner{
+peri=per;
+}
+
+function buyr() payable{
+if(msg.value<p)throw;
+end=block.number+peri;
+temp=msg.sender;
+myContract m=myContract(c);
+if(!c.transferOwnership(msg.sender))throw;
+}
+
+function claim() onlyOwner returns (bool){
+if(block.number<end)throw;
+myContract m=myContract(c);
+if(!c.transferOwnership(owner))throw;
+kill();
+}
+
+function withdraw(){
+if(msg.sender!=owner)throw;
+if(!send(msg.sender,this.balance))throw;
+}
+
+function kill() private{
+selfdestroy();
+}
+
+}
+
+
+contract myContract{ function transferOwnership(address newOwner)returns (bool){} }
