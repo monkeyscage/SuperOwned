4493297e16b7cd5e844ae057d823c86d19b18d5a
maxxflyer <maxxflyer@gmail.com>
2017-07-04 12:18:59 +0200

    Update r.sol

--- r.sol
+++ r.sol
@@ -1,12 +1,44 @@
-contract r{
+contract r is SuperOwned{
 address c;
+uint p;
+uint peri;
+uint end;
+address temp;
 
-function r(address con){
+function r(address con,uint pri,uint per){
 c=con;
+p=pri;
+peri=per;
 }
 
-function kill(){
+function setPrice(uint pri) onlyOwner{
+p=pri;
+}
+
+function setPeriod(uint per) onlyOwner{
+peri=per;
+}
+
+function buyr() payable{
+if(msg.value<p)throw;
+end=block.number+peri;
+temp=msg.sender;
+myContract m=myContract(c);
+if(!c.transferOwnership(msg.sender))throw;
+}
+
+function claim() onlyOwner returns (bool){
+if(block.number<end)throw;
+myContract m=myContract(c);
+if(!c.transferOwnership(owner))throw;
+kill();
+}
+
+function kill() private{
 selfdestroy();
 }
 
 }
+
+
+contract myContract{ function transferOwnership(address newOwner)returns (bool){} }
