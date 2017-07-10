7adb0c982fc9d1547a9063e06e5552f19696e786
maxxflyer <maxxflyer@gmail.com>
2017-07-10 15:18:18 +0200

    Update m.sol

--- m.sol
+++ m.sol
@@ -1,3 +1,35 @@
+contract SuperOwned{
+
+address public owner;
+address public superowner;
+
+    function SuperOwned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner { //superowner is more important than owner
+        if(superowner!=address(0)){
+        if (msg.sender != superowner) throw;
+        }else{
+        if (msg.sender != owner) throw;
+        }
+        _;
+    }
+
+    //you can transfer ownership only if NO superowner is declared
+    function transferOwnership(address newOwner) onlyOwner returns(bool){
+        owner = newOwner;
+        return true;
+    }
+    
+    //you can set a superowner only if NO superowner is already declared
+    function setSuperOwner(address newSuperOwner) onlyOwner {
+        superowner = newSuperOwner;    //can be an address or 0x0
+    }
+    
+
+}
+
 contract m is SuperOwned{
 address c;
 uint end;
